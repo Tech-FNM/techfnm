429ed00322725eed93d6c71ff926d5ceb271f971
Tabish Khan <tkla9988@gmail.com>
2026-08-22 15:26:04 +0500

    delete all public pages except homepage and request service page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
-import AboutPage from './pages/public/AboutPage';
-import ServicesPage from './pages/public/ServicesPage';
-import ServiceDetail from './pages/public/ServiceDetail';
-import PortfolioPage from './pages/public/PortfolioPage';
-import ContactPage from './pages/public/ContactPage';
-import FAQPage from './pages/public/FAQPage';
-import BlogPage from './pages/public/BlogPage';
-import BlogDetail from './pages/public/BlogDetail';
 import RequestServicePage from './pages/public/RequestServicePage';
 
 function App() {
@@ -15,14 +7,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/services" element={<ServicesPage />} />
-        <Route path="/services/:slug" element={<ServiceDetail />} />
-        <Route path="/portfolio" element={<PortfolioPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/faq" element={<FAQPage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/blog/:slug" element={<BlogDetail />} />
         <Route path="/request-service" element={<RequestServicePage />} />
         
         {/* Catch all */}
