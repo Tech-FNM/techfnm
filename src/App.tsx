8b11966318dae3355c9c5bdeed6c66b0888b5fb0
Tabish Khan <tkla9988@gmail.com>
2026-08-22 14:44:33 +0500

    remove admin dashboard and protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,6 @@ import BlogPage from './pages/public/BlogPage';
 import BlogDetail from './pages/public/BlogDetail';
 import RequestServicePage from './pages/public/RequestServicePage';
 
-import AdminLayout from './pages/admin/AdminLayout';
-import Login from './pages/admin/Login';
-import DashboardHome from './pages/admin/DashboardHome';
-import ServicesManager from './pages/admin/ServicesManager';
-import ProjectsManager from './pages/admin/ProjectsManager';
-import LeadershipManager from './pages/admin/LeadershipManager';
-import TestimonialsManager from './pages/admin/TestimonialsManager';
-import ClientsManager from './pages/admin/ClientsManager';
-import FAQManager from './pages/admin/FAQManager';
-import ScriptsManager from './pages/admin/ScriptsManager';
-import FooterManager from './pages/admin/FooterManager';
-import BlogManager from './pages/admin/BlogManager';
-import SeoManager from './pages/admin/SeoManager';
-import HomepageManager from './pages/admin/HomepageManager';
-import PagesContentManager from './pages/admin/PagesContentManager';
-import MediaManager from './pages/admin/MediaManager';
-import HeaderManager from './pages/admin/HeaderManager';
-import LeadsManager from './pages/admin/LeadsManager';
-import ProfileManager from './pages/admin/ProfileManager';
-import ProtectedRoute from './components/ProtectedRoute';
-
 function App() {
   return (
     <Router>
@@ -45,32 +24,6 @@ function App() {
         <Route path="/blog" element={<BlogPage />} />
         <Route path="/blog/:slug" element={<BlogDetail />} />
         <Route path="/request-service" element={<RequestServicePage />} />
-        
-        {/* Admin Routes */}
-        <Route path="/admindash/login" element={<Login />} />
-        
-        <Route element={<ProtectedRoute />}>
-          <Route path="/admindash" element={<AdminLayout />}>
-            <Route index element={<Navigate to="/admindash/dashboard" replace />} />
-            <Route path="dashboard" element={<DashboardHome />} />
-            <Route path="leads" element={<LeadsManager />} />
-            <Route path="services" element={<ServicesManager />} />
-            <Route path="projects" element={<ProjectsManager />} />
-            <Route path="blogs" element={<BlogManager />} />
-            <Route path="leadership" element={<LeadershipManager />} />
-            <Route path="testimonials" element={<TestimonialsManager />} />
-            <Route path="clients" element={<ClientsManager />} />
-            <Route path="faqs" element={<FAQManager />} />
-            <Route path="seo" element={<SeoManager />} />
-            <Route path="scripts" element={<ScriptsManager />} />
-            <Route path="footer" element={<FooterManager />} />
-            <Route path="header" element={<HeaderManager />} />
-            <Route path="homepage" element={<HomepageManager />} />
-            <Route path="pages" element={<PagesContentManager />} />
-            <Route path="media" element={<MediaManager />} />
-            <Route path="profile" element={<ProfileManager />} />
-          </Route>
-        </Route>
         
         {/* Catch all */}
         <Route path="*" element={<Navigate to="/" replace />} />
